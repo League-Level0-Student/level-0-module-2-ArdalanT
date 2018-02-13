d857efb351b7d7cb5cef213b83c068cd61c6c0db
league <league@wts21.attlocal.net>
2018-02-12 19:23:33 -0800

    COUNTDOWN, YEET!

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -13,16 +15,22 @@ package extra;
  **/
 
 public class NasaCountdown {
-	public static void main(String[] args) {
-		// 2. Use a pop-up to ask the user where to start counting from
+	public static void main(String[] args) throws InterruptedException {
+		// 2. Use a pop-up to ask the user where to start counting fromS
+		String input = JOptionPane.showInputDialog("WHERE SHOULD WE START COUNTING FROM");
 
 		// 3. Change the countdown to use the new starting point
-		
+		int x = Integer.parseInt(input);
 		// 1. Print a countdown from 10 to 0 on the console
+for(int i= x; i>=0; i-=1) {
 
 		// 4. Use the speak method to hear the countdown.
-		
+		JOptionPane.showMessageDialog(null, i);
+		speak(""+i);
+		Thread.sleep(1000);
+}
 		// 5. when the counting is done, speak "blastoff!"
+	speak("YEET!!!!!!");
 	}
 
 static void speak(String words) {
